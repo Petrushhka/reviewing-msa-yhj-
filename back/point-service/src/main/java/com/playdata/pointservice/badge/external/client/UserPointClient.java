4462f479fe25bd64be2bdf556d109b8f76ef7592
hyeonguk <shyunguk126@naver.com>
2025-05-09 16:30:21 +0900

    신국

--- back/point-service/src/main/java/com/playdata/pointservice/badge/external/client/UserPointClient.java
+++ back/point-service/src/main/java/com/playdata/pointservice/badge/external/client/UserPointClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 // user-service 이름은 Eureka 등록된 서비스명 또는 직접 URL
-@FeignClient(name = "user-service", configuration = FeignClient.class)
+@FeignClient(name = "user-service")
 public interface UserPointClient {
     @GetMapping("/user/{userId}/point")
     int getUserPoint(@PathVariable Long userId);
--- back/user-service/src/main/java/com/playdata/userservice/UserServiceApplication.java
+++ back/user-service/src/main/java/com/playdata/userservice/UserServiceApplication.java
@@ -2,13 +2,9 @@ package com.playdata.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-<<<<<<< HEAD
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients(basePackages = "com.playdata.userservice.user.external.client")
-=======
-
->>>>>>> 8536dc3db21a5aeb0534c647d23876583eaac35d
 @SpringBootApplication
 public class UserServiceApplication {
 
