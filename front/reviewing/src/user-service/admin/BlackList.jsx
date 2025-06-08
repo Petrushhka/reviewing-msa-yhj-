28a031b60f96f341b03d2206fd6cfd9b7d1c3cd1
hajoon <stravinskky@naver.com>
2025-06-08 18:26:27 +0900

    [User-servcie_Frontend] 1. 관리자가 유저 블락하는 페이지지 2. 블락된 유저 알람창

--- front/reviewing/src/user-service/admin/BlackList.jsx
+++ front/reviewing/src/user-service/admin/BlackList.jsx
@@ -12,15 +12,42 @@ const BlackList = () => {
     fetchUsers();
   }, []);
 
+  const token = localStorage.getItem('ACCESS_TOKEN');
+
   const fetchUsers = async () => {
     try {
-      const token = localStorage.getItem('ACCESS_TOKEN');
-      const res = await axios.get(`${API_BASE_URL}/admin/users`, {
+      const res = await axios.get(`${API_BASE_URL}/user-service/user-list`, {
         headers: { Authorization: `Bearer ${token}` },
       });
-      setUsers(res.data.result || []);
+      setUsers(res.data || []);
+      console.log(res.data);
     } catch (err) {
       console.error('회원 목록 불러오기 실패:', err);
+    }
+  };
+
+  const changeStatus = async (user) => {
+    try {
+      const res = await axios.patch(
+        `${API_BASE_URL}/user-service/change-status`,
+        {
+          userEmail: user.email,
+          isBlack: user.isBlack,
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      );
+
+      const updatedUsers = users.map((u) =>
+        u.email === user.email ? { ...u, isBlack: !u.isBlack } : u,
+      );
+      setUsers(updatedUsers);
+    } catch (e) {
+      console.error('실패', e);
     }
   };
 
@@ -49,14 +76,19 @@ const BlackList = () => {
             <th>상태</th>
           </tr>
         </thead>
-        <tbody>
+        <tbody className={styles.userList}>
           {filteredUsers.map((user) => (
             <tr key={user.id}>
               <td>{user.id}</td>
               <td>{user.nickName}</td>
               <td>{user.role}</td>
               <td>{user.email}</td>
-              <td>{user.status || '정상'}</td>
+              <td
+                className={`${styles.status} ${user.isBlack ? styles.status_blocked : ''}`}
+                onClick={() => changeStatus(user)}
+              >
+                {user.isBlack ? '정지' : '정상'}
+              </td>
             </tr>
           ))}
         </tbody>
